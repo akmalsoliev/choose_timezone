b69f76e048e5476517c1a0c47d86adef490a8d48
Akmal Soliev <soliev@gmail.com>
2020-12-13 22:39:15 +0100

    Added random.sample to avoid possibility of having the same choice

--- choose_timezone.py
+++ choose_timezone.py
@@ -2,28 +2,28 @@ import datetime
 import pytz
 import random
 
-given_timezones = []
-for names in range(0, 10):
-    given_timezones.append(pytz.all_timezones[random.randint(1, len(pytz.all_timezones))])
+number_of_inputs = 10 
+random_number = random.sample(range(0, len(pytz.all_timezones)), number_of_inputs)
+print(random_number)
 
 print('Hello and welcome to this small timezone checker application. Your options are:')
 print('''0. Exit''')
 
-for numbers in range(0, len(given_timezones)):
-    print('{}. {}'.format(numbers+1, given_timezones[numbers]))
+for numbers in range(0,number_of_inputs):
+    print('{}. {}'.format(numbers+1, pytz.all_timezones[random_number[numbers]]))
 
 while True:
     user_input = input('''Please choose the timezone you'd like to see.\n''')
     try:
         if 10 >= int(user_input) >= 1:
             user_input = int(user_input) - 1
-            chosen_timezone = pytz.timezone(given_timezones[user_input])
+            chosen_timezone = pytz.timezone(pytz.all_timezones[random_number[user_input]])
             wanted_timezone = datetime.datetime.now(tz=chosen_timezone)
             print('''Current time at: {} is {}:{}\nThe date is: {} of {}, {}'''.format(chosen_timezone, wanted_timezone.hour, wanted_timezone.minute, wanted_timezone.day, wanted_timezone.month, wanted_timezone.year))
             break
         elif int(user_input) == 0:
             break
         else:
-            print("The following option is not available. Please choose from 1 to {}".format(len(given_timezones)))
+            print("The following option is not available. Please choose from 1 to {}".format(number_of_inputs))
     except:
-        print("The following option is not available. Please choose from 1 to {}".format(len(given_timezones)))
+        print("The following option is not available. Please choose from 1 to {}".format(number_of_inputs))
