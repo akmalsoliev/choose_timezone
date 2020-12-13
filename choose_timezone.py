b333aa4c8fe275c223a97d3312e3e51776af47d4
Akmal Soliev <soliev@gmail.com>
2020-12-13 23:10:53 +0100

    code quality improvement

--- choose_timezone.py
+++ choose_timezone.py
@@ -4,7 +4,6 @@ import random
 
 number_of_inputs = 10 
 random_number = random.sample(range(0, len(pytz.all_timezones)), number_of_inputs)
-print(random_number)
 
 print('Hello and welcome to this small timezone checker application. Your options are:')
 print('''0. Exit''')
@@ -15,15 +14,22 @@ for numbers in range(0,number_of_inputs):
 while True:
     user_input = input('''Please choose the timezone you'd like to see.\n''')
     try:
-        if 10 >= int(user_input) >= 1:
-            user_input = int(user_input) - 1
-            chosen_timezone = pytz.timezone(pytz.all_timezones[random_number[user_input]])
+        user_input=int(user_input)
+        if 10 >= user_input >= 1:
+            user_input =- 1
+            index = random_number[user_input]
+            time_zone_name = pytz.all_timezones[index]
+
+            chosen_timezone = pytz.timezone(time_zone_name)
             wanted_timezone = datetime.datetime.now(tz=chosen_timezone)
             print('''Current time at: {} is {}:{}\nThe date is: {} of {}, {}'''.format(chosen_timezone, wanted_timezone.hour, wanted_timezone.minute, wanted_timezone.day, wanted_timezone.month, wanted_timezone.year))
             break
-        elif int(user_input) == 0:
+
+        elif user_input == 0:
             break
+
         else:
             print("The following option is not available. Please choose from 1 to {}".format(number_of_inputs))
+
     except:
         print("The following option is not available. Please choose from 1 to {}".format(number_of_inputs))
